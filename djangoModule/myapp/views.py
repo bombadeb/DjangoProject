036a4c944bc2cd3fd1af76012f4ba43327a7f031
bombadeb <bomba.deb@gmail.com>
2020-04-05 02:04:18 +0530

    Added build comments
    Added build comments

--- djangoModule/myapp/views.py
+++ djangoModule/myapp/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>This is the Home Page of the Applciation and the Build is 1.0.4</h1>')
+    return HttpResponse('<h1>This is the Home Page of the Applciation and the Build is 1.0.43 and Build number 43</h1>')
 
 # Create your views here.
