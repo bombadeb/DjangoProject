c82b2d4226b3f960d9a3f76a201e663641d4e05d
bombadeb <bomba.deb@gmail.com>
2020-04-06 10:44:14 +0530

    Update views.py

--- djangoModule/myapp/views.py
+++ djangoModule/myapp/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>This is the Home Page of the Applciation and the Build is 1.0.45 and Build number 45 and welcome</h1>')
+    return HttpResponse('<h1>This is the Home Page of the Applciation and the Build is 1.0.46 and Build number 46 and welcome</h1>')
 
 # Create your views here.
